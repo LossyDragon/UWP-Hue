3957fe068123ba4976fa7090359511ef8198494a
Lossy <thedragon1012@gmail.com>
2016-11-16 20:48:50 -0600

    Add project files.

---
+++ UWP-Hue/oobe.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+//TODO: Background image?
+
+namespace UWP_Hue
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class oobe : Page
+    {
+
+        public bool Bypass { get; private set; }
+
+        public oobe()
+        {
+            this.InitializeComponent();
+            //Function is called to see if HUE has been set-up or not.
+            CheckSetup();
+        }
+
+        private async Task CheckSetup()
+        {
+            try
+            {
+                //Does not work for saved files.
+                var folders = await ApplicationData.Current.LocalFolder.GetFoldersAsync();
+
+                if (folders.Count > 0)
+                {
+                    this.Frame.Navigate(typeof(Main));
+                }
+                
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+        }
+
+        //User Select oobe setup.
+        private void SetMeUp(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(oobeSetup));
+        }
+
+    }
+}
---
+++ UWP-Hue/oobeSetup.xaml.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using HueLibrary;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Windows.UI.Core;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+//Some code sampled from "HueLightController" on the MicrosoftUWP developer portal.
+
+//TODO: 1. Setup shit for hue
+//TODO: Find bridge
+//TODO: add failsafes per phillips SDK
+//TODO: proceed to 'main' app after connection. 
+
+namespace UWP_Hue
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    internal partial class oobeSetup
+    {
+        //Init
+        protected Frame rootFrame = new Frame();
+        private Bridge _bridge;
+        private IEnumerable<Light> _lights;
+
+        public oobeSetup()
+        {
+            this.InitializeComponent();
+
+        }
+
+        private async void PressedButton_Click(object sender, RoutedEventArgs e)
+        {
+            await FindBridgeAsync();
+            await FindLightsAsync();
+            SaveBridge();
+
+            //On Success, go to main UWP page.
+            await rootFrame.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                rootFrame.Navigate(typeof(Main),
+                    new HuePayload { Bridge = _bridge, Lights = _lights });
+                Window.Current.Content = rootFrame;
+            });
+        }
+
+        /// <summary>
+        /// Constructor for the initializer. This displays an extended splash screen and progress
+        /// ring while the app loads the bridge, lights, and Cortana. 
+        /// </summary>
+
+
+        /// <summary>
+        /// Tries to find the bridge using multiple methods.
+        /// http://www.developers.meethue.com/documentation/hue-bridge-discovery
+        /// </summary>
+        private async Task FindBridgeAsync()
+        {
+            try
+            {
+                // First attempt: local storage cache.
+                var localStorage = ApplicationData.Current.LocalSettings.Values;
+                Debug.WriteLine(localStorage);
+                if (localStorage.ContainsKey("bridgeIp") && localStorage.ContainsKey("userId"))
+                {
+                    _bridge = new Bridge(
+                        localStorage["bridgeIp"].ToString(),
+                        localStorage["userId"].ToString());
+                    if (await PrepareBridgeAsync())
+                    {
+                        return;
+                    }
+                }
+
+                // Second attempt: Hue N-UPnP service.
+                _bridge = await Bridge.FindAsync();
+                if (await PrepareBridgeAsync())
+                {
+                    return;
+                }
+
+                // Third attempt: Re-try Hue N-UPnP service.
+                await DispatchAwaitableUITask(async () =>
+                    await RetryBridgeSearchPopup.ShowAsync());
+                _bridge = await Bridge.FindAsync();
+                if (await PrepareBridgeAsync())
+                {
+                    return;
+                }
+
+                // Final attempt: manual entry.
+                await DispatchAwaitableUITask(async () =>
+                {
+                    await BridgeEntryPopup.ShowAsync();
+                    _bridge = new Bridge(BridgEntryIp.Text);
+                });
+                if (await PrepareBridgeAsync())
+                {
+                    return;
+                }
+            }
+            //Uh-Oh...
+            catch (Exception e)
+            {
+                await ReportErrorAsync(
+                    "We encountered an unexpected problem trying to find your bridge: " + e);
+            }
+            await ReportErrorAsync("We couldn't find your bridge. Make sure it's powered on, " +
+                "has 3 blue lights illuminated, on the same network as this device, " +
+                "and that you're connected to the Internet.");
+            this.Frame.Navigate(typeof(oobe));
+        }
+
+        /// <summary>
+        /// Saves bridge information to the app's local storage so the app can load faster next time. 
+        /// </summary>
+        private void SaveBridge()
+        {
+            var localStorage = ApplicationData.Current.LocalSettings.Values;
+            localStorage["bridgeIp"] = _bridge.Ip;
+            localStorage["userId"] = _bridge.UserId;
+        }
+
+        private Task DispatchAwaitableUITask(Func<Task> task)
+        {
+            var completion = new TaskCompletionSource<bool>();
+            var action = rootFrame.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+            {
+                try
+                {
+                    await task.Invoke();
+                    completion.TrySetResult(true);
+                }
+                catch (Exception e)
+                {
+                    completion.TrySetException(e);
+                }
+            });
+            return completion.Task;
+        }
+
+
+        /// <summary>
+        /// Checks whether the bridge is reachable and the app is authorized to send commands. If the bridge
+        /// is reachable but the app isn't authorized, it prompts the user to register it. 
+        /// </summary>
+        private async Task<bool> PrepareBridgeAsync(int attempts = 0)
+        {
+            if (null == _bridge || attempts > 2)
+            {
+                return false;
+            }
+            switch (await _bridge.PingAsync())
+            {
+                case BridgeConnectionStatus.Success:
+                    return true;
+                case BridgeConnectionStatus.Fail:
+                    return false;
+                case BridgeConnectionStatus.Unauthorized:
+                    await _bridge.RegisterAsync();
+                    return await PrepareBridgeAsync(++attempts);
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+
+        /// <summary>
+        /// Tries to find lights on the network.
+        /// </summary>
+        private async Task FindLightsAsync()
+        {
+            try
+            {
+                _lights = new ObservableCollection<Light>(await _bridge.GetLightsAsync());
+                if (!_lights.Any())
+                {
+                    await ReportErrorAsync("We couldn't find any lights. Make sure they're in " +
+                        "range and connected to a power source.");
+                }
+            }
+            catch (Exception e)
+            {
+                await ReportErrorAsync(
+                    "We encountered an unexpected problem trying to find your lights: " + e);
+            }
+        }
+
+
+        //Report an error Function...
+        private async Task ReportErrorAsync(string message)
+        {
+            await DispatchAwaitableUITask(async () =>
+            {
+                ErrorPopupText.Text = "Something went wrong.\r\n" +
+                    message + "\r\nThe app will now exit.";
+                var results = await ErrorPopup.ShowAsync();
+                Application.Current.Exit();
+            });
+        }
+
+        internal class HuePayload
+        {
+            public Bridge Bridge { get; set; }
+            public IEnumerable<Light> Lights { get; set; }
+        }
+    }
+}
